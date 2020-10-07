726240992973611498b1f8cd43985d00c1b2a378
Trinh Van Minh <trinhvanminh2009@gmai.com>
2020-10-07 14:47:26 +0700

    Init app

---
+++ lib/spin.dart
@@ -0,0 +1,65 @@
+
+import 'package:flutter/material.dart';
+
+class Spin extends StatefulWidget{
+  @override
+  _SpinState createState() => _SpinState();
+
+
+}
+
+class _SpinState extends State<Spin> with SingleTickerProviderStateMixin{
+  AnimationController controller;
+  Animation<double> animation;
+
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    controller = AnimationController(
+      duration: Duration(seconds:  10),
+      vsync: this,
+    );
+
+    animation =  CurvedAnimation(parent: controller, curve: Curves.bounceInOut).drive(Tween(begin: 0, end: 100));
+  }
+
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return GestureDetector(
+      onTap: () {
+        controller
+          ..reset()
+          ..forward();
+      },
+      child: RotationTransition(
+          turns: animation,
+          child: Stack(
+            children: [
+              Positioned.fill(
+                child: FlutterLogo(),
+              ),
+              Center(
+                child: Text('Tap here',
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold
+                )
+                )
+              )
+            ],
+          ),
+      ),
+    );
+  }
+
+}
